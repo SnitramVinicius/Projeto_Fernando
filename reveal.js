3da86bf974b68e8de742e287f7bbdc3c24b86f53
Vinicius Martins <costa.vinicius2010@gmail.com>
2024-09-25 16:04:23 -0400

    update

---
+++ reveal.js
@@ -0,0 +1,285 @@
+window.revelar = ScrollReveal({ reset: false });
+//index
+revelar.reveal('.efect-text-work',{
+    duration:2000, distance:'100px'
+})
+
+revelar.reveal('.efect-title-work',{
+    duration:2000, distance:'100px'
+})
+
+revelar.reveal('.efect-text2-work',{
+    duration:2000, distance:'100px'
+})
+
+revelar.reveal('.efeito-img-work',{
+    duration:2000, distance:'100px'
+})
+
+revelar.reveal('.efeito-text1',{
+    duration:2000, distance:'100px',
+    delay:100
+})
+revelar.reveal('.efeito-separation-bar1',{
+    duration:2000, distance:'100px',
+    delay:110
+})
+revelar.reveal('.efeito-text2',{
+    duration:2000, distance:'100px',
+    delay:150
+})
+revelar.reveal('.efeito-separation-bar2',{
+    duration:2000, distance:'100px',
+    delay:160
+})
+revelar.reveal('.efeito-text3',{
+    duration:2000, distance:'100px',
+    delay:200
+})
+revelar.reveal('.efeito-separation-bar3',{
+    duration:2000, distance:'100px',
+    delay:210
+})
+revelar.reveal('.efeito-text4',{
+    duration:2000, distance:'100px',
+    delay:250
+})
+revelar.reveal('.efeito-separation-bar4',{
+    duration:2000, distance:'100px',
+    delay:260
+})
+revelar.reveal('.efeito-text5',{
+    duration:2000, distance:'100px',
+    delay:300
+})
+revelar.reveal('.efeito-separation-bar5',{
+    duration:2000, distance:'100px',
+    delay:310
+})
+revelar.reveal('.efeito-text6',{
+    duration:2000, distance:'100px',
+    delay:350
+})
+revelar.reveal('.efeito-separation-bar6',{
+    duration:2000, distance:'100px',
+    delay:360
+})
+revelar.reveal('.efeito-text7',{
+    duration:2000, distance:'100px',
+    delay:400
+})
+revelar.reveal('.efeito-contato',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-contact',{
+    duration:2000, distance:'100px',
+    delay:250
+
+})
+revelar.reveal('.efeito-column',{
+    duration:2000, distance:'100px',
+    delay:500
+})
+revelar.reveal('.efeito-more-informations',{
+    duration:2000, distance:'100px',
+})
+revelar.reveal('.efeito-social-media',{
+    duration:2000, distance:'100px',
+    delay:500
+})
+
+//contato
+revelar.reveal('.efeito-informacoes',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-adv',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-nome',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-oab',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-space',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-email',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-h3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-email2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-contato',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-num1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-num2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-group1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-group2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-group3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-group4',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-group5',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-form-submit',{
+    duration:2000, distance:'100px'
+})
+//blog
+revelar.reveal('.efeito-container',{
+    duration:2000, distance:'100px'
+})
+//entrega
+revelar.reveal('.efeito-img-con1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-name',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-function',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-description',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-description2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-description3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeitoh1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeitop',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeitop1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeitop2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeitop3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeitop4',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-name2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-function2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-description',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-descriptionA',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-img-con2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-img-con3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-name3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-function3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-description3',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-description2',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-container-analise',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-analise-1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-column-analise',{
+    duration:2000, distance:'100px'
+})
+
+//topico1
+revelar.reveal('.efeito-t1',{
+    duration:2000, distance:'100px'
+})
+
+revelar.reveal('.efeito-h2-t1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-imgt1',{
+    duration:2000, distance:'100px'
+})
+revelar.reveal('.efeito-separation-bar-next',{
+    duration:2000, distance:'100px',
+    delay:1000
+})
+revelar.reveal('.efeito-next1',{
+    duration:2000, distance:'100px',
+    delay:1100
+})
+revelar.reveal('.efeito-separation-bar-next2',{
+    duration:2000, distance:'100px',
+    delay:1200
+})
+revelar.reveal('.efeito-next2',{
+    duration:2000, distance:'100px',
+    delay:1300
+})
+revelar.reveal('.efeito-separation-bar-next3',{
+    duration:2000, distance:'100px',
+    delay:1400
+})
+
+//topico2
+revelar.reveal('.efeitoh2',{
+    duration:2000, distance:'100px',
+  
+})
+revelar.reveal('.efeito-p',{
+    duration:2000, distance:'100px',
+    
+})
+revelar.reveal('.efeito-img1t2',{
+    duration:2000, distance:'100px',
+  
+})
+revelar.reveal('.efeito-li',{
+    duration:2000, distance:'100px',
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const swiper = new Swiper('.slider-wrapper', {
-    loop: true,
-    grapCursor: true,
-    spaceBetween:4,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets:true
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  loop: true,
+  grapCursor: true,
+  spaceBetween:4,
 
-    breakpoints: {
-        0:{
-            slidersPerView:1
-        },
-        768:{
-            slidersPerView:2
-        },
-        1024:{
-            slidersPerView:3
-        },
-    }
-  });
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      0:{
+          slidersPerView:1
+      },
+      768:{
+          slidersPerView:2
+      },
+      1024:{
+          slidersPerView:3
+      },
+  }
+});
